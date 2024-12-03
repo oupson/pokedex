bfef9749b9ada7ee3c6b322859e0e39dd7ea4ef3
Anthony Froissant <froissant@users.noreply.github.com>
2024-12-03 11:55:55 +0100

    Add healthcheck

--- pokeapi/server.js
+++ pokeapi/server.js
@@ -2,6 +2,13 @@ import Fastify from 'fastify'
 import fastifySensible from '@fastify/sensible'
 import pokemons from './routes/pokemons.js';
 
+const HEALTHCHECK_ENDPOINT = process.env.HEALTHCHECK_ENDPOINT || "/health";
+
+const POKEDEX_NAME = process.env.POKEDEX_NAME;
+if(typeof POKEDEX_NAME === "undefined") {
+    throw new Error("POKEDEX_NAME must be set");
+}
+
 const fastify = Fastify({
     logger: true
 });
@@ -15,6 +22,10 @@ fastify.get('/', (request, reply) => {
         .type('text/html')
         .send(`<h1>Welcome to the ${process.env.POKEDEX_NAME} Pok&eacute;dex API!</h1>`);
 });
+
+fastify.get(HEALTHCHECK_ENDPOINT, { logLevel: 'silent' }, (request, reply) => {
+    reply.send({ status: "Healthy" });
+})
 
 fastify.register(pokemons);
 
